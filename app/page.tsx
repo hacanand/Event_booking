fd772be03ea475f4eb9da3cdc81630197dd98348
anand <anandpromax[at]gmail[dot]com>
2025-01-02 15:30:45 +0530

    project setup and api creation

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+     
+    <div className="flex flex-col p-10 justify-center items-center">
+      <h1>Home</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
