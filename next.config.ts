511a4107544b39a0deafd5177996c9c6d3700550
devnouiq <dev@nouiq.com>
2025-01-17 20:57:57 +0530

    ignore lint

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true, // Ignore TypeScript errors
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore ESLint errors
+  },
   distDir: 'build',
   output: 'standalone',
 };
